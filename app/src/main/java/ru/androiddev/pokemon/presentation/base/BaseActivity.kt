4cf0f40828a1a894aeebf5193765275157f5a5f2
Maxim Kutushev <zip2pac@mail.ru>
2022-05-31 11:44:28 +0300

    Remove injection from activity.

--- app/src/main/java/ru/androiddev/pokemon/presentation/base/BaseActivity.kt
+++ app/src/main/java/ru/androiddev/pokemon/presentation/base/BaseActivity.kt
@@ -3,14 +3,9 @@ package ru.androiddev.pokemon.presentation.base
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import dagger.android.AndroidInjection
-import javax.inject.Inject
 
 abstract class BaseActivity<F : ViewModelProvider.Factory, V : BaseViewModel> : AppCompatActivity() {
-
-    @Inject
-    lateinit var viewModelFactory: F
 
     lateinit var viewModel: V
 
@@ -21,8 +16,4 @@ abstract class BaseActivity<F : ViewModelProvider.Factory, V : BaseViewModel> : 
     }
 
     abstract fun initViewModel()
-
-    protected fun setViewModel(klass: Class<V>) {
-        viewModel = ViewModelProviders.of(this, viewModelFactory)[klass]
-    }
 }
