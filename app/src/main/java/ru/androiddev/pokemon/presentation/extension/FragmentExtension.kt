27f60fec12d5ad6916f440d0a01aacec3a6dfe01
Maxim Kutushev <zip2pac@mail.ru>
2022-06-29 22:02:04 +0300

    Create add fragment to backstack method.

--- app/src/main/java/ru/androiddev/pokemon/presentation/extension/FragmentExtension.kt
+++ app/src/main/java/ru/androiddev/pokemon/presentation/extension/FragmentExtension.kt
@@ -10,3 +10,11 @@ fun FragmentActivity.addFragment(fragment: Fragment, @IdRes container: Int) {
         .add(container, fragment, fragment.javaClass.name)
         .commit()
 }
+
+fun FragmentActivity.addFragmentToBackstack(fragment: Fragment, @IdRes container: Int) {
+    supportFragmentManager
+        .beginTransaction()
+        .replace(container, fragment, fragment.javaClass.name)
+        .addToBackStack(fragment.javaClass.name)
+        .commit()
+}
